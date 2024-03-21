fd2c167196758ca24274aa3ec861e7e33cca348f
bjoel2 <bjoel2@i2pmail.org>
2024-03-21 15:37:57 +0300

    added char-only serializer

--- Serialize.hpp
+++ Serialize.hpp
@@ -3,10 +3,29 @@ namespace safini
 namespace serialize
 {
 
+//char type concept(see stdc++20 for more info)
+template<typename CharType>
+concept Character = std::same_as<CharType, char>
+                 || std::same_as<CharType, unsigned char>;
+
 //converts passed string_view to the AnyTypeStorage that contains SerializedType
-//for integer types only
+//for char types only
+template<typename SerializedType>
+constexpr auto getSerizlizeFunc() requires Character<SerializedType>
+{
+    return [](const std::string_view str)->AnyTypeStorage
+    {
+        if(str.size() != 1) throw std::runtime_error("not a character");
+
+        AnyTypeStorage toReturn(std::in_place_type_t<SerializedType>{}, str[0]);
+        return toReturn;
+    };
+}
+
+//for integer types only, excluding char types
 template<typename SerializedType>
 constexpr auto getSerizlizeFunc() requires std::is_integral_v<SerializedType>
+                                        && (!Character<SerializedType>) //means not character concept
 {
     return [](const std::string_view str)->AnyTypeStorage
     {
