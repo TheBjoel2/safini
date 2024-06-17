8ecf90fb37e49fecb5ece0d993bc97b186fc2c3c
bjoel2 <bjoel2@i2pmail.org>
2024-06-18 01:24:24 +0300

    better string to integer serializer

--- Serialize.hpp
+++ Serialize.hpp
@@ -1,3 +1,5 @@
+#include <limits>
+
 namespace safini
 {
 namespace serialize
@@ -15,21 +17,47 @@ constexpr auto getSerizlizeFunc() requires Character<SerializedType>
 {
     return [](const std::string_view str)->AnyTypeStorage
     {
-        if(str.size() != 1) throw std::runtime_error("not a character");
+        if(str.size() != 1) throw std::runtime_error("convertible string is not a character");
 
         AnyTypeStorage toReturn(std::in_place_type_t<SerializedType>{}, str[0]);
         return toReturn;
     };
 }
 
-//for integer types only, excluding char types
+//for signed integer types only, excluding char types
 template<typename SerializedType>
 constexpr auto getSerizlizeFunc() requires std::is_integral_v<SerializedType>
+                                        && std::is_signed_v<SerializedType>
                                         && (!Character<SerializedType>) //means not character concept
 {
     return [](const std::string_view str)->AnyTypeStorage
     {
-        AnyTypeStorage toReturn(std::in_place_type_t<SerializedType>{}, std::stoll(std::string(str)));
+        const int64_t conversionResult = std::stoll(std::string(str));
+
+        const bool flag1 = conversionResult >= std::numeric_limits<SerializedType>::min();
+        const bool flag2 = conversionResult <= std::numeric_limits<SerializedType>::max();
+        if(!flag1 || !flag2) throw std::out_of_range("convertible number out of range");
+
+        AnyTypeStorage toReturn(std::in_place_type_t<SerializedType>{}, static_cast<SerializedType>(conversionResult));
+        return toReturn;
+    };
+}
+
+//for unsigned integer types only, excluding char types
+template<typename SerializedType>
+constexpr auto getSerizlizeFunc() requires std::is_integral_v<SerializedType>
+                                        && std::is_unsigned_v<SerializedType>
+                                        && (!Character<SerializedType>) //means not character concept
+{
+    return [](const std::string_view str)->AnyTypeStorage
+    {
+        const uint64_t conversionResult = std::stoull(std::string(str));
+
+        const bool flag1 = conversionResult >= std::numeric_limits<SerializedType>::min();
+        const bool flag2 = conversionResult <= std::numeric_limits<SerializedType>::max();
+        if(!flag1 || !flag2) throw std::out_of_range("convertible number out of range");
+
+        AnyTypeStorage toReturn(std::in_place_type_t<SerializedType>{}, static_cast<SerializedType>(conversionResult));
         return toReturn;
     };
 }
